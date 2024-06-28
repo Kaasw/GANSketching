45ee99495ac143d849a6f856f0c8850459db769c
Kaasw <hieu1282003@gmail.com>
2024-06-28 18:35:54 +0700

    Change lsun dataset

--- data/download_lsun.sh
+++ data/download_lsun.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 mkdir -p image/lmdb
-wget http://dl.yf.io/lsun/objects/cat.zip -O ./data/image/cat.zip
-wget http://dl.yf.io/lsun/objects/horse.zip -O ./data/image/horse.zip
-wget http://dl.yf.io/lsun/scenes/church_outdoor_train_lmdb.zip -O ./data/image/church.zip
-unzip ./data/image/cat.zip -d ./data/image/lmdb
-unzip ./data/image/horse.zip -d ./data/image/lmdb
-unzip ./data/image/church.zip -d ./data/image/lmdb
-python ./data/prepare_lsun.py ./data/image/cat ./data/image/lmdb/cat
-python ./data/prepare_lsun.py ./data/image/horse ./data/image/lmdb/horse
-python ./data/prepare_lsun.py ./data/image/church ./data/image/lmdb/church_outdoor_train_lmdb
+wget "https://vnueduvn-my.sharepoint.com/:u:/g/personal/21021493_vnu_edu_vn/ESepXt3K-gxNrC5loM0k1X8B0OiG55negoXgl7w5qZ3JuA?e=lelUUi&download=1" -O ./data/image/baldeagle.zip
+
+unzip ./data/image/baldeagle.zip -d ./data/image/lmdb
+python ./data/prepare_lsun.py ./data/image/baldeagle ./data/image/lmdb/baldeagle
+
+
 rm -r ./data/image/lmdb
-rm ./data/image/cat.zip
-rm ./data/image/horse.zip
-rm ./data/image/church.zip
+rm ./data/image/baldeagle.zip
+
