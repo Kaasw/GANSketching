1fe370354a2593139c00476617e07383313d4b51
Kaasw <hieu1282003@gmail.com>
2024-06-28 18:50:02 +0700

    adjust download_lsun

--- data/download_lsun.sh
+++ data/download_lsun.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p image/lmdb
+mkdir -p data/image
 wget "https://vnueduvn-my.sharepoint.com/:u:/g/personal/21021493_vnu_edu_vn/ESepXt3K-gxNrC5loM0k1X8B0OiG55negoXgl7w5qZ3JuA?e=lelUUi&download=1" -O ./data/image/baldeagle.zip
 
 unzip ./data/image/baldeagle.zip -d ./data/image/lmdb
